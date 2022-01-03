62f77c8025342be17fe51bac878d1cf9368beebd
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-01-03 23:44:26 +0530

    created delete option

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from crypt import methods
-
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-# from models import Book
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Books.db'
@@ -22,35 +19,45 @@ db.create_all()
 all_books = []
 
 
-@app.route("/update", methods=["GET", "POST"])
-def update_rating(id: int):
-    rating_to_update = Book.query.get(id)
-    print(rating_to_update)
-    if request.method == "GET":
-        return render_template("update.html", book=rating_to_update)
-    else:
-        rating_to_update.rating = request.form["new_rating"]
-        db.session.commit()
-        return redirect(url_for("home"))
-
-
 @app.route('/')
 def home():
+    if request.args.get("id"):
+        delete_id = request.args.get("id")
+        delete_book = Book.query.get(delete_id)
+        db.session.delete(delete_book)
+        db.session.commit()
+        return redirect(url_for("home"))
     books = db.session.query(Book).all()
     return render_template("index.html", books=books)
 
 
+@app.route("/edit", methods=["GET", "POST"])
+def update_rating():
+    if request.method == "POST":
+        book_id = request.form["post_id"]
+        update_book = Book.query.get(book_id)
+        update_book.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    get_id = request.args.get("post_id")
+    book_selected = Book.query.get(get_id)
+    return render_template("update.html", book=book_selected)
+
+
 @app.route("/add", methods=['GET', 'POST'])
 def add():
-    if request.method == 'POST':
-        all_books.append(dict(request.form))
-        new_book = Book(title=request.form["title"], author=request.form["author"], rating=request.form["rating"])
-        db.session.add(new_book)
-        db.session.commit()
+    try:
+        if request.method == 'POST':
+            all_books.append(dict(request.form))
+            new_book = Book(title=request.form["title"], author=request.form["author"], rating=request.form["rating"])
+            db.session.add(new_book)
+            db.session.commit()
+            return redirect(url_for("home"))
+    except:
         return redirect(url_for("home"))
     return render_template("add.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
